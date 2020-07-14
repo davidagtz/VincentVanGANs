07d118007e01e5a2f2288360eca54dd8a5393d84
David Gutierrez <david.a.gutierrez99@gmail.com>
2020-07-14 02:07:17 -0500

    Added variables and data normalization

--- vincentVanGANs.py
+++ vincentVanGANs.py
@@ -1,46 +1,66 @@
 import tensorflow as tf
+import generator as gen
+import discriminator as dis
+import matplotlib.pyplot as plot
+import numpy as np
 from tensorflow import keras
 from os import listdir
-import generator as gen
-import discriminator as discrim
-import matplotlib.pyplot as plot
+from os.path import join
 from VanImages import refresh_dir
 from argparse import ArgumentParser
+from PIL import Image
+
+parser = ArgumentParser(
+    description="Makes and trains GANs for painting generation")
+parser.add_argument("--refresh", nargs=1)
+args = parser.parse_args()
+
+# Main Config
+OUTDIR = "res"
+EPOCHS = 25
+IMAGE_URLS = listdir(OUTDIR)
+TRAINING_SIZE = len(IMAGE_URLS)
+BATCH = TRAINING_SIZE // 32
+
+INPUT_SIZE = 100
+# Width * Height * Channels
+INPUT_SHAPE = (128, 128, 3)
+
+if args.refresh != None:
+    shape = (int(args.refresh[0]), int(args.refresh[0]), 3)
+    if shape[0] / 32. % 1 != 0:
+        raise Exception("Edges not multiples of 32")
+    INPUT_SHAPE = shape
+    refresh_dir(OUTDIR, (shape[0], shape[0]))
+
+# Get Data
+data = []
+for url in IMAGE_URLS:
+    image = Image.open(join(OUTDIR, url))
+    data.append(np.asarray(image))
+# Reshapes data to be (data_size, img, img, channel)
+data = np.reshape(data, (-1, INPUT_SHAPE[0], INPUT_SHAPE[0], 3))
+# Pixel values from -1 to 1
+data = data.astype(np.float32)
+data = data / 127.5 - 1
+
+# Make models
+generator = gen.model(INPUT_SHAPE, INPUT_SIZE)
+discriminator = dis.model(INPUT_SHAPE)
+
+# The optimizers that will adjust the models
+gen_optimizer = keras.optimizers.Adam(1.3e-4, .48)
+dis_optimizer = keras.optimizers.Adam(1.3e-4, .48)
 
 
-def get_args():
-    parser = ArgumentParser(
-        description="Makes and trains GANs for Van Gogh Painting generation")
-
-    parser.add_argument("--refresh", nargs=2, default=None, required=False)
-
-    return parser.parse_args()
-
-# optimizer = keras.optimizers.Adam(1.5e-4, 0.5)
+# Compiled function
+@tf.function
+def step(img_list):
+    pass
 
 
-def genTest(generator):
-    noise = tf.random.normal([1, INPUT_SIZE])
-
-    gen_img = generator(noise, training=False)[0, :, :, 0]
-    return plot.imshow(gen_img)
+def train(img_list, epochs):
+    pass
 
 
-if __name__ == "__main__":
-    INPUT_SIZE = 100
-    # Width * Height * Channels
-    INPUT_SHAPE = (128, 128, 3)
-
-    args = get_args()
-    if args.refresh != None:
-        shape = (int(args.refresh[0]), int(args.refresh[1]), 3)
-        if shape[0] / 32. % 1 != 0 or shape[0] != shape[1]:
-            raise Exception("Either bounds are not square or a multiple of 32")
-        INPUT_SHAPE = shape
-        refresh_dir("res", (shape[0], shape[1]))
-
-    generator = gen.model(INPUT_SHAPE, INPUT_SIZE)
-    discriminator = discrim.model(INPUT_SHAPE)
-
-    image = genTest(generator)
-    plot.show()
+train(data, EPOCHS)
